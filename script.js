a4a668f3e978711f8678647dd09cb8c8e324c1b8
Ritik <ritikpatel705@gmail.com>
2023-08-02 18:35:23 +0530

    added

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function loadMovies(searchTerm){
     const res = await fetch(`${URL}`);
     const data = await res.json();
     // console.log(data.Search);
-    if(data.Response == "true") displayMovieList(data.Search);
+    if(data.Response == "True") displayMovieList(data.Search);
 }
 
 function findMovies(){
@@ -49,10 +49,10 @@ function displayMovieList(movies){
     loadMovieDetails();
 }
 
-function loadMovieDetails(){
+async function loadMovieDetails(){
     const searchListMovies = searchList.querySelectorAll('.search-list-item');
     searchListMovies.forEach(movie => {
-        movie.addEventListener('click', async() => {
+        movie.addEventListener('click', async () => {
             // console.log(movie.dataset.id);
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
